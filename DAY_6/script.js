249120fc5df583db9aadd08a910bfc46740a1200
bnaveenbharathi <naveenbharathi5050@gmail.com>
2025-03-27 22:47:15 +0530

    weather application init

---
+++ DAY_6/script.js
@@ -0,0 +1,12 @@
+const array=['mark','mark2','mark3',2]
+const object={
+    name:'mark',
+    age:19
+}
+
+console.log(array);
+console.log(object);
+
+for(let i=0;i<array.length;i++){
+    console.log(array[i]);
+}
