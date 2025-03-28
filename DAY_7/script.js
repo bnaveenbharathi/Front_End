9ef557e9acee529a43c1cf01a0034c51f7f9b33b
bnaveenbharathi <naveenbharathi5050@gmail.com>
2025-03-28 19:08:19 +0530

    callback working

--- DAY_7/script.js
+++ DAY_7/script.js
@@ -1,49 +1,24 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const form = document.querySelector('.weather-form');
-    const conditions = document.querySelector('.condition');
-    const temperature = document.querySelector('#temperature');
-    const conditionImg = document.querySelector('.conditionimg');
-    const placeCon = document.querySelector('.placecon');
-    const countryCon = document.querySelector('#country');
-    const timeContent = document.querySelector('#time');
+// var x = 5;
 
+// console.log(z); 
+// let z = 5;
 
-    form.addEventListener("submit", (event) => {
-        event.preventDefault();
+// console.log(m); 
+// const m = 5;
 
-        const apiKey = 'd50043b9ac704398b23122901242107';
-        const baseUrl = 'https://api.weatherapi.com/v1/current.json';
-        const location = document.querySelector('.location').value
+// greet();
+// function greet() {
+//   console.log("Hello, Mark!");
+// }
 
-        const url = `${baseUrl}?key=${apiKey}&q=${location}`;
+let value = 1;
 
-        fetch(url)
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
-                }
-                return response.json();
-            })
-            .then(data => {
-                const icon = data.current.condition.icon;
-                const condition = data.current.condition.text;
-                const temp = data.current.temp_c;
-                const country = data.location.country;
-                const location = data.location.name;
-                const time = data.location.localtime;
+function doSomething(call){
+    call()
+}
 
-
-                conditionImg.src = icon;
-                timeContent.innerHTML = `<b>Time:</b> ${time}`;
-                countryCon.innerHTML = `<b>Country:</b> ${country}`;
-                conditions.textContent = condition;
-                temperature.textContent = `${temp}°C`;
-                placeCon.textContent = location;
-
-
-            })
-            .catch(error => {
-                console.error('Fetch error:', error);
-            });
-    });
+let some = doSomething(() => {
+  value = 2;
 });
+
+console.log(value); 
