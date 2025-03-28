6cfee477fc85e57e9b78bf560497fe62f829c7e8
bnaveenbharathi <naveenbharathi5050@gmail.com>
2025-03-28 19:08:37 +0530

    callback working

--- DAY_6/script.js
+++ DAY_6/script.js
@@ -1,12 +1,28 @@
-const array=['mark','mark2','mark3',2]
-const object={
-    name:'mark',
-    age:19
-}
+// const array=['mark','mark2','mark3',2]
+// const object={
+//     name:'mark',
+//     age:19
+// }
 
-console.log(array);
-console.log(object);
+// console.log(array);
+// console.log(object);
 
-for(let i=0;i<array.length;i++){
-    console.log(array[i]);
+// for(let i=0;i<array.length;i++){
+//     console.log(array[i]);
+// }
+
+
+
+// console.log(x); 
+// var x = 5;
+
+// console.log(z); 
+// let z = 5;
+
+// console.log(m); 
+// const m = 5;
+
+greet();
+function greet() {
+  console.log("Hello, Mark!");
 }
