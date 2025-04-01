09ae332ca5a02084b9d22abd61c28afec74ad6ef
bnaveenbharathi <naveenbharathi5050@gmail.com>
2025-04-01 20:58:08 +0530

    day 10 tasks

---
+++ DAY_10/main.js
@@ -0,0 +1,132 @@
+
+
+
+// function main(){
+//     var a=5
+//     console.log(a);
+// }
+
+// main()
+// console.log(a);
+
+// if(true){
+//     var z=30
+// }
+
+// var z=20
+
+// console.log(z);
+
+// for(let i=0;i<3;i++){
+// (
+//     function (j){
+//         setTimeout(()=>{
+//             console.log(j);
+//         },1000)
+//     }
+// )(i)
+// }
+
+
+
+// {
+//     let a=5
+//     console.log(a);
+// }
+// console.log(a);
+
+// for(var i=0;i<5;i++){
+//  setTimeout(()=>{
+//   console.log(i);
+//  },1000)
+// }
+// console.log(i);
+
+// {
+//      let i_escape="leaked bro"
+// console.log(i_escape);
+   
+// }
+// console.log(i_escape);
+
+
+// function leaked(){
+//    const leak=2
+// }
+// leaked()
+// console.log(leak);
+
+
+// let change='mark'
+
+// function main(){
+//      let change ='mark2'
+//     console.log(change);// mark2
+// }
+// main()
+// console.log(change);//mark
+
+//Write a C program to find maximum between three numbers.
+// let value1=23
+// let value2=20
+// let value3=30
+// if(value1>value2 && value1>value3) {
+//     console.log(value1);
+// }else if(value2>value3 && value2>value1){
+//    console.log(value2);
+// }else{
+//     console.log(value3);
+// }
+
+
+//----------------------------
+//Write a C program to check whether a number is negative, positive or zero.
+// let value=0
+
+// if(value >0){
+//     console.log("possitive");
+// }else if(value==0){
+//     console.log('0');
+// }
+// else{
+//     console.log('neggative');
+// }
+
+//------------------------------
+//check whether a number is divisible by 5 and 11 or not
+// let value=55;
+
+// if((value%5)===0 && value%11===0){
+//     console.log("divibale by 5 and 11");
+// }else{
+//     console.log("not");
+// }
+
+
+// print day of week name using switch case
+
+// switch(3){
+//     case 1:
+//         console.log("monday");
+//         break;
+//     case 2:
+//         console.log('tuesday');
+//         break;
+//     case 3:
+//         console.log("wed");
+//         break;
+//     case 4:
+//         console.log('thursday');
+//         break;
+//     case 5:
+//         console.log('friday');
+//         break;
+//     case 6:
+//         console.log('saturday');
+//         break
+//     default:
+//         console.log("wrong value");
+    
+// }
+
+
